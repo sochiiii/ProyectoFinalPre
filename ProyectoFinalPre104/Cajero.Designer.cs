b9c66d644514b47bee3c1eb48b8e5c3fcd63f874
Gerardo Cruz <CA250809@alumno.udb.edu.sv>
2025-08-15 17:54:31 -0600

    (soy tato) Finalize la funcionalidad de mi formulario, era el N3, el del cajero

--- ProyectoFinalPre104/Cajero.Designer.cs
+++ ProyectoFinalPre104/Cajero.Designer.cs
@@ -30,20 +30,35 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.calcaj = new System.Windows.Forms.Button();
+            this.limpcaj = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.din = new System.Windows.Forms.TextBox();
+            this.txt20 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt100 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(167, 225);
+            this.label1.Location = new System.Drawing.Point(202, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(316, 20);
+            this.label1.Size = new System.Drawing.Size(253, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Aquí va el ejercicio 3 del cajero de banco";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(582, 338);
+            this.button1.Location = new System.Drawing.Point(1220, 409);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(134, 59);
             this.button1.TabIndex = 1;
@@ -51,14 +66,166 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // calcaj
+            // 
+            this.calcaj.Location = new System.Drawing.Point(357, 188);
+            this.calcaj.Name = "calcaj";
+            this.calcaj.Size = new System.Drawing.Size(98, 46);
+            this.calcaj.TabIndex = 2;
+            this.calcaj.Text = "Calcular";
+            this.calcaj.UseVisualStyleBackColor = true;
+            this.calcaj.Click += new System.EventHandler(this.calcaj_Click);
+            // 
+            // limpcaj
+            // 
+            this.limpcaj.Location = new System.Drawing.Point(500, 188);
+            this.limpcaj.Name = "limpcaj";
+            this.limpcaj.Size = new System.Drawing.Size(98, 46);
+            this.limpcaj.TabIndex = 3;
+            this.limpcaj.Text = "Limpiar";
+            this.limpcaj.UseVisualStyleBackColor = true;
+            this.limpcaj.Click += new System.EventHandler(this.limpcaj_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingresa la cantidad de dinero a retirar";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // din
+            // 
+            this.din.Location = new System.Drawing.Point(280, 125);
+            this.din.Name = "din";
+            this.din.Size = new System.Drawing.Size(118, 22);
+            this.din.TabIndex = 5;
+            // 
+            // txt20
+            // 
+            this.txt20.Enabled = false;
+            this.txt20.Location = new System.Drawing.Point(152, 297);
+            this.txt20.Name = "txt20";
+            this.txt20.Size = new System.Drawing.Size(100, 22);
+            this.txt20.TabIndex = 6;
+            // 
+            // txt10
+            // 
+            this.txt10.Enabled = false;
+            this.txt10.Location = new System.Drawing.Point(152, 339);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(100, 22);
+            this.txt10.TabIndex = 7;
+            // 
+            // txt5
+            // 
+            this.txt5.Enabled = false;
+            this.txt5.Location = new System.Drawing.Point(152, 381);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 22);
+            this.txt5.TabIndex = 8;
+            // 
+            // txt1
+            // 
+            this.txt1.Enabled = false;
+            this.txt1.Location = new System.Drawing.Point(152, 421);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 22);
+            this.txt1.TabIndex = 9;
+            // 
+            // txt100
+            // 
+            this.txt100.Enabled = false;
+            this.txt100.Location = new System.Drawing.Point(152, 257);
+            this.txt100.Name = "txt100";
+            this.txt100.Size = new System.Drawing.Size(100, 22);
+            this.txt100.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Recibira la cantidad de billetes de: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Billetes de $100";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Billetes de $20";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Billetes de $10";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 381);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Billetes de $5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 424);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Billetes de $1";
+            // 
             // Cajero
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1384, 494);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt100);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt20);
+            this.Controls.Add(this.din);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.limpcaj);
+            this.Controls.Add(this.calcaj);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Cajero";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cajero";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -69,5 +236,20 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button calcaj;
+        private System.Windows.Forms.Button limpcaj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox din;
+        private System.Windows.Forms.TextBox txt20;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt100;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- ProyectoFinalPre104/Cajero.cs
+++ ProyectoFinalPre104/Cajero.cs
@@ -23,5 +23,81 @@ namespace ProyectoFinalPre104
             ventana.Show();
             this.Hide();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcaj_Click(object sender, EventArgs e)
+        {
+            if (din.Text == "")
+            {
+                MessageBox.Show("Debe ingresar algun monto a retirar", "Error");
+                din.Focus();
+                return;
+            }
+
+            int data;
+            try
+            {
+                data = Convert.ToInt32(din.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe ingresar un valor monetario numerico ", "Error");
+                din.Text = "";
+                din.Focus();
+                return;
+            }
+
+            //Calculos
+            int a, b, c, d, cien, vein, die, cinc, uno;
+
+            cien = data / 100;
+            a = data % 100;
+
+            vein = a / 20;
+            b = a % 20;
+
+            die = b / 10;
+            c = b % 10;
+
+            cinc = c / 5;
+            d = c % 5;
+
+            uno = d / 1;
+
+            //Mostrar resultados
+            txt100.Text = cien.ToString();
+            txt20.Text = vein.ToString();
+            txt10.Text = die.ToString();
+            txt5.Text = cinc.ToString();
+            txt1.Text = uno.ToString();
+            
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void limpcaj_Click(object sender, EventArgs e)
+        {
+            din.Text = "";
+            txt100.Text = "";
+            txt20.Text = "";
+            txt10.Text = "";
+            txt5.Text = "";
+            txt1.Text = "";
+            din.Focus();
+        }
     }
 }
