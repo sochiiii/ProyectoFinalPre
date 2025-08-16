eff143d6ba66ac1cef8d763cbea0ab677ed53de4
Wilson Cruz <wilsonalcidescrulopez@gmail.com>
2025-08-16 11:58:29 -0600

    *Creé una carpeta "assets" para guardar los elementos multimedia que podríamos utilizar en nustros formularios. *Comencé a modificar mi formulario de angulos agudos

--- ProyectoFinalPre104/Angulos.Designer.cs
+++ ProyectoFinalPre104/Angulos.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Angulos));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -35,18 +36,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(278, 132);
+            this.label1.Location = new System.Drawing.Point(15, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 16);
+            this.label1.Size = new System.Drawing.Size(148, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Aquí va lo de los ángulos";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(545, 332);
+            this.button1.Location = new System.Drawing.Point(545, 312);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 41);
+            this.button1.Size = new System.Drawing.Size(165, 39);
             this.button1.TabIndex = 1;
             this.button1.Text = "Volver al menú";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,11 +57,15 @@
             // 
             // Angulos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 422);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Angulos";
             this.Text = " ";
             this.ResumeLayout(false);
