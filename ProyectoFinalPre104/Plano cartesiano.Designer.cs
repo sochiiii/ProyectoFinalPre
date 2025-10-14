9ea58d24f12595978866e2e68ff92e0b282c39e8
Wilson Cruz <wilsonalcidescrulopez@gmail.com>
2025-10-14 16:47:05 -0600

    modifiqué el manejo del punto para usar vectores y no una clase

--- ProyectoFinalPre104/Plano cartesiano.Designer.cs
+++ ProyectoFinalPre104/Plano cartesiano.Designer.cs
@@ -31,6 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.lbl_planno = new System.Windows.Forms.Label();
             this.point = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_planno
@@ -51,11 +57,70 @@
             this.point.Size = new System.Drawing.Size(10, 10);
             this.point.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(548, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(479, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(318, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese los valores de las coordenadas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(620, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(620, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(548, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Y:";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(864, 461);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.point);
             this.Controls.Add(this.lbl_planno);
             this.Font = new System.Drawing.Font("Berlin Sans FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -65,6 +130,7 @@
             this.Text = "Plano cartesiano";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +138,11 @@
 
         private System.Windows.Forms.Label lbl_planno;
         private System.Windows.Forms.Label point;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- ProyectoFinalPre104/Plano cartesiano.cs
+++ ProyectoFinalPre104/Plano cartesiano.cs
@@ -12,15 +12,35 @@ namespace ProyectoFinalPre104
 {
     public partial class Form2 : Form
     {
-        static Point origen = new Point(202, 243); // Punto de origen en la esquina superior izquierda
+        // Punto de origen en el centro del plano cartesiano
+        static Double[] origen = { 202, 243 };
+
+        // Matriz para almacenar la escala del plano cartesiano
+        static Double[] escala = { 10.0, 10.0 };
 
         // Matriz para almacenar las divisiones del plano cartesiano
         static Double[,] divisiones = {
             {-10.0, -7.5, - 5.0, - 2.5, 0, 2.5, 5.0, 7.5, 10.0},
             {-10.0, -7.5, -5.0, -2.5, 0, 2.5, 5.0, 7.5, 10.0}
         };
+
+        // Conversiones de unidades
         static Double x_dimTopx = 50.6;
         static Double y_dimTopx = 44.0;
+
+        //Función para ubicar un label en el plano cartesiano
+        static void ubicarLabel(Label lbl, Double x, Double y)
+        {
+            // Calcular la posición en píxeles
+            int px = (int)(origen[0] + (x * x_dimTopx / 2.5));
+            int py = (int)(origen[1] - (y * y_dimTopx / 2.5));
+            // Ajustar la posición del label
+            lbl.Location = new Point(px - lbl.Width / 2, py - lbl.Height / 2);
+        }
+
+        //Coordenadas de entrada del usuario
+        static Double[] p_in = { 0, 0 };
+
         public Form2()
         {
             InitializeComponent();
@@ -35,32 +55,43 @@ namespace ProyectoFinalPre104
         {
 
         }
-    }
 
-    //Clase para representar un punto en el plano cartesiano
-    public class Punto
-    {
-        // Coordenadas del punto
-        public Double x;
-        public Double y;
-
-        // Constructor
-        public Punto(Double x, Double y)
+        private void label2_Click(object sender, EventArgs e)
         {
-            this.x = x;
-            this.y = y;
+
         }
 
-        // Método para representar el punto como una cadena
-        public override string ToString()
+        private void label1_Click(object sender, EventArgs e)
         {
-            return $"({x}, {y})";
+
         }
 
-        // Método para obtener el punto
-        public Punto GetPoint()
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            return this;
+            //Verifica si se ingresó un número válido
+            if (!Double.TryParse(textBox1.Text, out Double x))
+            {
+                //Clarar el valor de x como 0 si la entrada no es válida
+                if (!String.IsNullOrEmpty(textBox1.Text))
+                {
+                    MessageBox.Show("Por favor, ingrese solo números válidos en el campo de la coordenada X.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else 
+                {
+                    x = 0;
+                    p_in[0] = x;
+                    escala[0] = 10.0;
+                    ubicarLabel(point, p_in[0], p_in[1]);
+                }
+            }
+            else
+            {
+                p_in[0] = x;
+                if (Math.Abs(x) <= 10) escala[0] = 10;
+                else if (Math.Abs(x) > 10 && Math.Abs(x) <= 100) escala[0] = 100;
+                else escala[0] = Math.Round(Math.Abs(x) + 1);
+                ubicarLabel(point, p_in[0], p_in[1]);
+            }
         }
     }
 }
