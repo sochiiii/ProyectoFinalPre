9b8c994bee5a580551184c1f76e9ae20b09237f9
sochiiii <20220018@ricaldone.edu.sv>
2025-10-21 19:23:29 -0600

    pantalla babosa -_-

--- ProyectoFinalPre104/Herencia.Designer.cs
+++ ProyectoFinalPre104/Herencia.Designer.cs
@@ -28,7 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.her1 = new System.Windows.Forms.TextBox();
+            this.juanx = new System.Windows.Forms.TextBox();
+            this.luisx = new System.Windows.Forms.TextBox();
+            this.rosax = new System.Windows.Forms.TextBox();
+            this.abogado = new System.Windows.Forms.TextBox();
+            this.calc = new System.Windows.Forms.Button();
+            this.limp = new System.Windows.Forms.Button();
+            this.menuclk = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // her1
+            // 
+            this.her1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.her1.Font = new System.Drawing.Font("Berlin Sans FB", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.her1.Location = new System.Drawing.Point(63, 278);
+            this.her1.Name = "her1";
+            this.her1.Size = new System.Drawing.Size(183, 27);
+            this.her1.TabIndex = 8;
+            // 
+            // juanx
+            // 
+            this.juanx.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.juanx.Enabled = false;
+            this.juanx.Font = new System.Drawing.Font("Berlin Sans FB", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.juanx.Location = new System.Drawing.Point(393, 207);
+            this.juanx.Name = "juanx";
+            this.juanx.Size = new System.Drawing.Size(164, 27);
+            this.juanx.TabIndex = 9;
+            // 
+            // luisx
+            // 
+            this.luisx.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.luisx.Enabled = false;
+            this.luisx.Font = new System.Drawing.Font("Berlin Sans FB", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luisx.Location = new System.Drawing.Point(657, 207);
+            this.luisx.Name = "luisx";
+            this.luisx.Size = new System.Drawing.Size(169, 27);
+            this.luisx.TabIndex = 10;
+            // 
+            // rosax
+            // 
+            this.rosax.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.rosax.Enabled = false;
+            this.rosax.Font = new System.Drawing.Font("Berlin Sans FB", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rosax.Location = new System.Drawing.Point(532, 278);
+            this.rosax.Name = "rosax";
+            this.rosax.Size = new System.Drawing.Size(169, 27);
+            this.rosax.TabIndex = 11;
+            // 
+            // abogado
+            // 
+            this.abogado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.abogado.Enabled = false;
+            this.abogado.Font = new System.Drawing.Font("Berlin Sans FB", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.abogado.Location = new System.Drawing.Point(678, 361);
+            this.abogado.Name = "abogado";
+            this.abogado.Size = new System.Drawing.Size(135, 27);
+            this.abogado.TabIndex = 12;
+            // 
+            // calc
+            // 
+            this.calc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.calc.Font = new System.Drawing.Font("Gloucester MT Extra Condensed", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calc.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.calc.Location = new System.Drawing.Point(22, 432);
+            this.calc.Margin = new System.Windows.Forms.Padding(4);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(119, 54);
+            this.calc.TabIndex = 14;
+            this.calc.Text = "CALCULAR";
+            this.calc.UseVisualStyleBackColor = false;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // limp
+            // 
+            this.limp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.limp.Font = new System.Drawing.Font("Gloucester MT Extra Condensed", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limp.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.limp.Location = new System.Drawing.Point(164, 432);
+            this.limp.Margin = new System.Windows.Forms.Padding(4);
+            this.limp.Name = "limp";
+            this.limp.Size = new System.Drawing.Size(119, 54);
+            this.limp.TabIndex = 15;
+            this.limp.Text = "LIMPIAR";
+            this.limp.UseVisualStyleBackColor = false;
+            this.limp.Click += new System.EventHandler(this.limp_Click);
+            // 
+            // menuclk
+            // 
+            this.menuclk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(231)))), ((int)(((byte)(201)))));
+            this.menuclk.Font = new System.Drawing.Font("Gloucester MT Extra Condensed", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuclk.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.menuclk.Location = new System.Drawing.Point(707, 423);
+            this.menuclk.Margin = new System.Windows.Forms.Padding(4);
+            this.menuclk.Name = "menuclk";
+            this.menuclk.Size = new System.Drawing.Size(119, 54);
+            this.menuclk.TabIndex = 16;
+            this.menuclk.Text = "REGRESAR";
+            this.menuclk.UseVisualStyleBackColor = false;
+            this.menuclk.Click += new System.EventHandler(this.menuclk_Click);
             // 
             // Herencia
             // 
@@ -37,6 +136,14 @@
             this.BackgroundImage = global::ProyectoFinalPre104.Properties.Resources.fondoHerencia;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(870, 520);
+            this.Controls.Add(this.menuclk);
+            this.Controls.Add(this.limp);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.abogado);
+            this.Controls.Add(this.rosax);
+            this.Controls.Add(this.luisx);
+            this.Controls.Add(this.juanx);
+            this.Controls.Add(this.her1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -44,9 +151,19 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Herencia";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox her1;
+        private System.Windows.Forms.TextBox juanx;
+        private System.Windows.Forms.TextBox luisx;
+        private System.Windows.Forms.TextBox rosax;
+        private System.Windows.Forms.TextBox abogado;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Button limp;
+        private System.Windows.Forms.Button menuclk;
     }
 }
--- ProyectoFinalPre104/Herencia.cs
+++ ProyectoFinalPre104/Herencia.cs
@@ -16,5 +16,91 @@ namespace ProyectoFinalPre104
         {
             InitializeComponent();
         }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            //Esto se ejecutara si no pone nada en el dato de la harencia
+            if (her1.Text == "")
+            {
+                MessageBox.Show("Debe ingresar algun monto a retirar", "Error");
+                her1.Focus();
+                return;
+            }
+            //Esto se ejecutara si el dato de la Herencia no son numeros
+            Double data;
+            try
+            {
+                data = Convert.ToDouble(her1.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe ingresar un valor monetario numerico ", "Error");
+                her1.Text = "";
+                her1.Focus();
+                return;
+            }
+
+            Double a, b, c;
+            Double tres, sum1, sum2, sum3, total;
+            //Calculos
+            a = data / 3; //Formula para la herencia de Juan
+            b = (4 * data) / 9; //Formula para la herencia de Luis
+            c = (2 * data) / 9; //Formula para la herencia de Rosa
+                                //Calculo para el abogado
+            tres = data / 3;
+            if (a < tres)
+            {
+                sum1 = a * 0.05;
+            }
+            else
+            {
+                sum1 = a * 0.03;
+            }
+
+
+            if (b < tres)
+            {
+                sum2 = b * 0.05;
+            }
+            else
+            {
+                sum2 = b * 0.03;
+            }
+
+            if (c < tres)
+            {
+                sum3 = b * 0.05;
+            }
+            else
+            {
+                sum3 = c * 0.03;
+            }
+            total = sum1 + sum2 + sum3;
+
+
+            //Aqui declaro que las respuesta a, b y c se impriman en los espacios indicados
+            juanx.Text = Math.Round(a, 2).ToString();
+            luisx.Text = Math.Round(b, 2).ToString();
+            rosax.Text = Math.Round(c, 2).ToString();
+            abogado.Text = Math.Round(total, 2).ToString();
+        }
+
+        private void limp_Click(object sender, EventArgs e)
+        {
+            juanx.Text = "";
+            luisx.Text = "";
+            rosax.Text = "";
+            abogado.Text = "";
+            her1.Text = "";
+            her1.Focus();
+        }
+
+        private void menuclk_Click(object sender, EventArgs e)
+        {
+            Form1 ventana = new Form1();
+            ventana.Show();
+            this.Close();
+            this.Dispose();
+        }
     }
 }
